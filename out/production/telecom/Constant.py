8ca0ac744840f0874905af61384fe5b85c509476
KatouMegumiMoe <lzykun@mail.ustc.edu.cn>
2018-09-24 11:19:42 +0800

    remove out

--- out/production/telecom/Constant.py
+++
@@ -1,8 +0,0 @@
-import Const
-
-
-Const.TRAIN_FILE_NAME = '../resources/train.csv'
-Const.TEST_FILE_NAME = '../resources/test.csv'
-Const.RESULT_FILE_NAME = '../resources/result.csv'
-Const.SUBMISSION_FILE_NAME = '../resources/submission.csv'
-Const.NUM_ROUND = 200
